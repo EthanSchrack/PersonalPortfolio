941aeb6d2137e3494d55910e37ca31919dbf6f73
Ethan Schrack <ethanschrack@gmail.com>
2024-05-13 17:13:46 -0400

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import "./Header.scss";
+
+function Header() {
+
+    return (
+        <div className="header-container">
+            <div className="header-item">Welcome</div>
+            <div className="header-item">About</div>
+            <div className="header-item">Projects</div>
+            <div className="header-item">Contact</div>
+        </div>
+    );
+}
+
+export default Header;
